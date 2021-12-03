6c78f09833772d2f906ba10643f0e5bb47b44bcf
werbliben <werbliben@gmail.com>
2021-12-03 20:12:33 +0100

    Updated core.py with d-core implementation

--- networkx/algorithms/core.py
+++ networkx/algorithms/core.py
@@ -41,6 +41,9 @@ __all__ = [
     "k_corona",
     "k_truss",
     "onion_layers",
+    "d_core_number",
+    "find_d_cores",
+    "d_core"
 ]
 
 
@@ -113,9 +116,6 @@ def core_number(G):
                 bin_boundaries[core[u]] += 1
                 core[u] -= 1
     return core
-
-
-find_cores = core_number
 
 
 def _core_subgraph(G, k_filter, k=None, core=None):
@@ -195,6 +195,168 @@ def k_core(G, k=None, core_number=None):
         return c[v] >= k
 
     return _core_subgraph(G, k_filter, k, core_number)
+
+
+#######################################
+# DIRECTED CORE DECOMPOSITION BABYYYY #
+#######################################
+
+
+def d_core_number(G):
+    """Returns the core number for each vertex.
+
+    A k-core is a maximal subgraph that contains nodes of degree k or more.
+
+    The core number of a node is the largest value k of a k-core containing
+    that node.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+       A graph or directed graph
+
+    Returns
+    -------
+    d_core_number : dictionary
+       A dictionary keyed by node to the core number.
+
+    Raises
+    ------
+    NetworkXError
+        The d-core is not implemented for graphs with self loops
+        or parallel edges or undirected graphs.
+
+    Notes
+    -----
+    Not implemented for graphs with parallel edges or self loops.
+
+    Only node out-degree is considered. For k-cores use core_number(G) node degree is defined to be the
+    in-degree + out-degree.
+
+    References
+    ----------
+    .. [1] An O(m) Algorithm for Cores Decomposition of Networks
+       Vladimir Batagelj and Matjaz Zaversnik, 2003.
+       https://arxiv.org/abs/cs.DS/0310049
+    """
+    if nx.number_of_selfloops(G) > 0 or nx.is_directed(G) == False:
+        msg = (
+            "Input graph has self loops which is not permitted; "
+            "Consider using G.remove_edges_from(nx.selfloop_edges(G))."
+        )
+        raise NetworkXError(msg)
+
+    out_degrees = dict(G.out_degree())
+    # Sort nodes by degree.
+    nodes = sorted(out_degrees, key=out_degrees.get)
+    bin_boundaries = [0]
+    curr_degree = 0
+    for i, v in enumerate(nodes):
+        if out_degrees[v] > curr_degree:
+            bin_boundaries.extend([i] * (out_degrees[v] - curr_degree))
+            curr_degree = out_degrees[v]
+    node_pos = {v: pos for pos, v in enumerate(nodes)}
+    # The initial guess for the core number of a node is its degree.
+    core = out_degrees
+    nbrs = {v: list(nx.all_neighbors(G, v)) for v in G}
+    for v in nodes:
+        for u in nbrs[v]:
+            if core[u] > core[v]:
+                nbrs[u].remove(v)
+                pos = node_pos[u]
+                bin_start = bin_boundaries[core[u]]
+                node_pos[u] = bin_start
+                node_pos[nodes[bin_start]] = pos
+                nodes[bin_start], nodes[pos] = nodes[pos], nodes[bin_start]
+                bin_boundaries[core[u]] += 1
+                core[u] -= 1
+    return core
+
+
+find_d_cores = d_core_number
+
+
+def _d_core_subgraph(G, d_filter, d=None, core=None):
+    """Returns the subgraph induced by nodes passing filter `d_filter`.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+       The directed graph to process
+    d_filter : filter function
+       This function filters the nodes chosen. It takes three inputs:
+       A node of G, the filter's cutoff, and the core dict of the graph.
+       The function should return a Boolean value.
+    d : int, optional
+      The order of the directed core. If not specified use the max core number.
+      This value is used as the cutoff for the filter.
+    core : dict, optional
+      Precomputed core numbers keyed by node for the graph `G`.
+      If not specified, the core numbers will be computed from `G`.
+
+    """
+    if core is None:
+        core = d_core_number(G)
+    if d is None:
+        d = max(core.values())
+    nodes = (v for v in core if d_filter(v, d, core))
+    return G.subgraph(nodes).copy()
+
+
+def d_core(G, d=None, d_core_number=None):
+    """Returns the k-core of G.
+
+    A k-core is a maximal subgraph that contains nodes of degree d or more.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+      A graph or directed graph
+    d : int, optional
+      The order of the core.  If not specified return the main core.
+    d_core_number : dictionary, optional
+      Precomputed directed core numbers for the graph G.
+
+    Returns
+    -------
+    G : NetworkX graph
+      The k-core subgraph
+
+    Raises
+    ------
+    NetworkXError
+      The k-core is not defined for graphs with self loops or parallel edges.
+
+    Notes
+    -----
+    The main core is the core with the largest degree.
+
+    Not implemented for graphs with parallel edges or self loops.
+
+    For directed graphs the node degree is defined to be the
+    in-degree + out-degree.
+
+    Graph, node, and edge attributes are copied to the subgraph.
+
+    See Also
+    --------
+    d_core_number
+
+    References
+    ----------
+    .. [1] An O(m) Algorithm for Cores Decomposition of Networks
+       Vladimir Batagelj and Matjaz Zaversnik,  2003.
+       https://arxiv.org/abs/cs.DS/0310049
+    """
+
+    def d_filter(v, d, c):
+        return c[v] >= d
+
+    return _d_core_subgraph(G, d_filter, d, d_core_number)
+
+####################################
+# DIRECTED CORE DECOMPOSITION OVER #
+####################################
 
 
 def k_shell(G, k=None, core_number=None):
